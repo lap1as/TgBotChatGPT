c5de526fa38ab851455a0055815abf43fef6a04c
lap1as <kuzukoleg12@gmail.com>
2023-01-28 21:05:02 +0100

    git

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import logging
+import openai
+import json
+from aiogram import Bot, Dispatcher, executor, types
+
+from dotenv import load_dotenv
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+
+telegram_api = os.environ.get("TELEGRAM_API")
+openai.api_key = os.environ.get("OPENAI_API")
+
+
+bot = Bot(token=telegram_api)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def send_welcome(message: types.Message):
+    print(f"[+]",{message.from_id},":",message.text)
+    prompt = (f"{message.text}")
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.8
+    )
+    generated = response.choices[0].text
+    await message.answer(text=generated)
+    
+    
+if __name__ == '__main__':
+    executor.start_polling(dp)
